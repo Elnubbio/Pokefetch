1dadba3f43f8ce48c375a544bda7588c67884af1
Dan <120772114+Elnubbio@users.noreply.github.com>
2023-03-01 03:25:50 +0000

    Update inquire.js
    App finished, just requires clean up.

--- inquire.js
+++ inquire.js
@@ -1,6 +1,7 @@
 import inquirer from "inquirer";
 
 const promptInput = async () => {
+    // console.log("Arrived at promptInput");
     const question = [
         {
             type: "input",
@@ -12,36 +13,38 @@ const promptInput = async () => {
         },
     ];
     const answer = await inquirer.prompt(question);
-    console.log(answer.userInput);
-    return answer.userInput;
+    // console.log(answer.userInput);
+    return await answer.userInput;
 };
 
 //returns a selection of ["Artwork", "Sprites", "Stats"]
 const promptOptions = async () => {
-    const banana = await promptInput();
-    inquirer
-        .prompt([
-            {
-                type: "checkbox",
-                message: "Select what you want to fetch:",
-                name: "File options",
-                choices: [
-                    new inquirer.Separator(" = Choices = "),
-                    {
-                        name: "Artwork",
-                    },
-                    {
-                        name: "Sprites",
-                    },
-                    {
-                        name: "Stats",
-                    },
-                ],
-            },
-        ])
-        .then((answers) => {
-            console.log(answers["File options"]);
-            return answers["File options"];
-        });
+    // console.log("Arrived at promptOptions");
+    const question = [
+        {
+            type: "checkbox",
+            message: "Select what you want to fetch:",
+            name: "File options",
+            choices: [
+                new inquirer.Separator(" = Choices = "),
+                {
+                    name: "Artwork",
+                },
+                {
+                    name: "Sprites",
+                },
+                {
+                    name: "Stats",
+                },
+            ],
+        },
+    ];
+    const answer = await inquirer.prompt(question);
+    // console.log(answer["File options"]);
+    return await answer["File options"];
+    // .then((answers) => {
+    //     console.log(answers["File options"]);
+    //     return answers["File options"];
+    // });
 };
-promptOptions();
+export { promptOptions, promptInput };
